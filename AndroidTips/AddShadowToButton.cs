5d4d2e7f04c6ce272f3fec9b9d9ae8a22ab3ec5f
logesh <logesh@hakunamatata.in>
2020-09-18 22:30:56 +0530

    added shadow button sample

---
+++ AndroidTips/AddShadowToButton.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace AndroidTips
+{
+    [Activity(Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class AddShadowToButton : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.shadow_layout);
+        }
+    }
+}
